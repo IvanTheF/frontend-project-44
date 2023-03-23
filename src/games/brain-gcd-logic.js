e5eee37ab106edd86592e007c32391cd9c3a6001
Ivan Tonkikh <vanya.tonkikh666@gmail.com>
2023-03-23 19:08:00 +0300

    add brain-gcd game

---
+++ src/games/brain-gcd-logic.js
@@ -0,0 +1,29 @@
+import startGame from "../index.js";
+import randomNum from "../utils.js";
+
+const instruction = "Find the greatest common divisor of given numbers.";
+
+const getGcd = (firstNumber, secondNumber) => {
+  let gcd;
+
+  for (let i = 1; i <= firstNumber && i <= secondNumber; i += 1) {
+    if (firstNumber % i == 0 && secondNumber % i == 0) {
+      gcd = i;
+    }
+  }
+  return gcd;
+};
+
+const generateRoundGcd = () => {
+  const firstNumber = randomNum(1, 100);
+  const secondNumber = randomNum(1, 100);
+
+  const question = `${firstNumber} ${secondNumber}`;
+  const rightAnswer = String(getGcd(firstNumber, secondNumber));
+
+  return [question, rightAnswer];
+};
+
+const startGcdGame = () => startGame(instruction, generateRoundGcd);
+
+export default startGcdGame;
