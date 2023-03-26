01796486fbe6eb2630e249cd09af4f20adf8061a
Ivan Tonkikh <vanya.tonkikh666@gmail.com>
2023-03-26 19:04:35 +0300

    fix brain-even

--- src/games/brain-even-logic.js
+++ src/games/brain-even-logic.js
@@ -4,13 +4,10 @@ import startGame from "../index.js";
 const instruction =
   'Answer "yes" if the number is even, otherwise answer "no".';
 
-const minNumber = 1;
-const maxnumber = 100;
-
 const evenNum = (number) => number % 2 === 0;
 
 const generateRoundData = () => {
-  const number = randomNum(minNumber, maxnumber);
+  const number = randomNum(1, 100);
   const question = number;
   const rightAnswer = evenNum(number) ? "yes" : "no";
   return [question, rightAnswer];
